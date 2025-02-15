aec2852f21ced4c5bc05133b092229e151c38f87
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2025-02-16 01:06:23 +0600

    setup biome and format files

--- consumer/index.ts
+++ consumer/index.ts
@@ -2,59 +2,70 @@ import amqp from "amqplib";
 import Dockerode from "dockerode";
 
 const docker = new Dockerode({
-    host: '127.0.0.1',
-    port: 2375,
-    protocol: 'http'
-})
+	host: "127.0.0.1",
+	port: 2375,
+	protocol: "http",
+});
 
 const startContainer = async (key: string): Promise<{ statusCode: Number }> => {
-    const container = await docker.createContainer({
-        Image: "transcoder",
-        HostConfig: {
-            NetworkMode: "vt",
-        },
-        Tty: true,
-        Env: [
-            "S3_REGION=us-east-1",
-            "S3_ENDPOINT=http://s3mock:9090",
-            "S3_ACCESS_KEY_ID=test",
-            "S3_ACCESS_SECRET=test",
-            `KEY=${key}`,
-            "TEMP_BUCKET=temp",
-            "PROD_BUCKET=prod",
-        ]
-    })
+	const container = await docker.createContainer({
+		Image: "transcoder",
+		HostConfig: {
+			NetworkMode: "vt",
+		},
+		Tty: true,
+		Env: [
+			"S3_REGION=us-east-1",
+			"S3_ENDPOINT=http://s3mock:9090",
+			"S3_ACCESS_KEY_ID=test",
+			"S3_ACCESS_SECRET=test",
+			`KEY=${key}`,
+			"TEMP_BUCKET=temp",
+			"PROD_BUCKET=prod",
+		],
+	});
 
-    await container.start()
-    console.log("[x] Container started")
+	await container.start();
+	console.log("[x] Container started");
 
-    return await container.wait()
-}
+	return await container.wait();
+};
 
-export const consumeMessages = async (queue: string, exchange: string, routing_key: string) => {
-    const connection = await amqp.connect("amqp://localhost:5672")
-    const channel = await connection.createChannel();
-    await channel.assertQueue(queue, { durable: false })
+export const consumeMessages = async (
+	queue: string,
+	exchange: string,
+	routing_key: string,
+) => {
+	const connection = await amqp.connect("amqp://localhost:5672");
+	const channel = await connection.createChannel();
+	await channel.assertQueue(queue, { durable: false });
 
-    console.log(`[*] Waiting for messages (Exchange: ${exchange}, Routing Key: ${routing_key})`);
+	console.log(
+		`[*] Waiting for messages (Exchange: ${exchange}, Routing Key: ${routing_key})`,
+	);
 
-    channel.consume(queue, async (msg) => {
-        if (msg) {
-            console.log(`[x] Received ${msg.content}`)
-            const data = JSON.parse(msg.content.toString()) as { key: string }
+	channel.consume(
+		queue,
+		async (msg) => {
+			if (msg) {
+				console.log(`[x] Received ${msg.content}`);
+				const data = JSON.parse(msg.content.toString()) as { key: string };
 
-            try {
-                const res = await startContainer(data.key)
-                if (res.statusCode === 0) {
-                    channel.ack(msg)
-                } else {
-                    channel.nack(msg)
-                }
-            } catch (err) {
-            }
-        }
-    }, { noAck: false });
+				try {
+					const res = await startContainer(data.key);
+					if (res.statusCode === 0) {
+						channel.ack(msg);
+					} else {
+						channel.nack(msg);
+					}
+				} catch (err) {}
+			}
+		},
+		{ noAck: false },
+	);
 };
 
 // Usage
-consumeMessages("upload_queue", "upload_exchange", "file.uploaded").catch(console.error);
+consumeMessages("upload_queue", "upload_exchange", "file.uploaded").catch(
+	console.error,
+);
--- src/uploader.ts
+++ src/uploader.ts
@@ -1,32 +1,36 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import { readFileSync } from "fs"
+import { readFileSync } from "fs";
 import { randomUUID } from "crypto";
 import { produce } from "./rmq/producer";
 import path from "path";
 
-
 const client = new S3Client({
-    region: "us-east-1",
-    endpoint: 'http://localhost:9090',
-    forcePathStyle: true,
-    credentials: {
-        accessKeyId: 'test',
-        secretAccessKey: 'test'
-    }
-})
+	region: "us-east-1",
+	endpoint: "http://localhost:9090",
+	forcePathStyle: true,
+	credentials: {
+		accessKeyId: "test",
+		secretAccessKey: "test",
+	},
+});
 
 const upload = async () => {
-    const filePath = path.join(__dirname, "assets", "video.mp4")
-    const fileStream = readFileSync(filePath)
-    const Key = randomUUID() + ".mp4"
-    const command = new PutObjectCommand({
-        Bucket: "temp",
-        Key: Key,
-        Body: fileStream,
-        ContentType: 'video/mp4',
-    })
-    await client.send(command)
-    produce(JSON.stringify({ key: Key }), "upload_queue", "upload_exchange", "file.uploaded")
-}
+	const filePath = path.join(__dirname, "assets", "video.mp4");
+	const fileStream = readFileSync(filePath);
+	const Key = randomUUID() + ".mp4";
+	const command = new PutObjectCommand({
+		Bucket: "temp",
+		Key: Key,
+		Body: fileStream,
+		ContentType: "video/mp4",
+	});
+	await client.send(command);
+	produce(
+		JSON.stringify({ key: Key }),
+		"upload_queue",
+		"upload_exchange",
+		"file.uploaded",
+	);
+};
 
-upload()
+upload();
