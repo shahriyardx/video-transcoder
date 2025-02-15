60c9d3212e39342523500b5384381a71ae5764dc
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2025-02-16 01:08:42 +0600

    fix lint errors

--- consumer/index.ts
+++ consumer/index.ts
@@ -1,13 +1,13 @@
-import amqp from "amqplib";
-import Dockerode from "dockerode";
+import amqp from "amqplib"
+import Dockerode from "dockerode"
 
 const docker = new Dockerode({
 	host: "127.0.0.1",
 	port: 2375,
 	protocol: "http",
-});
+})
 
-const startContainer = async (key: string): Promise<{ statusCode: Number }> => {
+const startContainer = async (key: string): Promise<{ statusCode: number }> => {
 	const container = await docker.createContainer({
 		Image: "transcoder",
 		HostConfig: {
@@ -23,49 +23,49 @@ const startContainer = async (key: string): Promise<{ statusCode: Number }> => {
 			"TEMP_BUCKET=temp",
 			"PROD_BUCKET=prod",
 		],
-	});
+	})
 
-	await container.start();
-	console.log("[x] Container started");
+	await container.start()
+	console.log("[x] Container started")
 
-	return await container.wait();
-};
+	return await container.wait()
+}
 
 export const consumeMessages = async (
 	queue: string,
 	exchange: string,
 	routing_key: string,
 ) => {
-	const connection = await amqp.connect("amqp://localhost:5672");
-	const channel = await connection.createChannel();
-	await channel.assertQueue(queue, { durable: false });
+	const connection = await amqp.connect("amqp://localhost:5672")
+	const channel = await connection.createChannel()
+	await channel.assertQueue(queue, { durable: false })
 
 	console.log(
 		`[*] Waiting for messages (Exchange: ${exchange}, Routing Key: ${routing_key})`,
-	);
+	)
 
 	channel.consume(
 		queue,
 		async (msg) => {
 			if (msg) {
-				console.log(`[x] Received ${msg.content}`);
-				const data = JSON.parse(msg.content.toString()) as { key: string };
+				console.log(`[x] Received ${msg.content}`)
+				const data = JSON.parse(msg.content.toString()) as { key: string }
 
 				try {
-					const res = await startContainer(data.key);
+					const res = await startContainer(data.key)
 					if (res.statusCode === 0) {
-						channel.ack(msg);
+						channel.ack(msg)
 					} else {
-						channel.nack(msg);
+						channel.nack(msg)
 					}
 				} catch (err) {}
 			}
 		},
 		{ noAck: false },
-	);
-};
+	)
+}
 
 // Usage
 consumeMessages("upload_queue", "upload_exchange", "file.uploaded").catch(
 	console.error,
-);
+)
--- src/uploader.ts
+++ src/uploader.ts
@@ -1,8 +1,8 @@
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import { readFileSync } from "fs";
-import { randomUUID } from "crypto";
-import { produce } from "./rmq/producer";
-import path from "path";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
+import { randomUUID } from "node:crypto"
+import { produce } from "./rmq/producer"
+import { readFileSync } from "node:fs"
+import path from "node:path"
 
 const client = new S3Client({
 	region: "us-east-1",
@@ -12,25 +12,25 @@ const client = new S3Client({
 		accessKeyId: "test",
 		secretAccessKey: "test",
 	},
-});
+})
 
 const upload = async () => {
-	const filePath = path.join(__dirname, "assets", "video.mp4");
-	const fileStream = readFileSync(filePath);
-	const Key = randomUUID() + ".mp4";
+	const filePath = path.join(__dirname, "assets", "video.mp4")
+	const fileStream = readFileSync(filePath)
+	const Key = randomUUID()
 	const command = new PutObjectCommand({
 		Bucket: "temp",
 		Key: Key,
 		Body: fileStream,
 		ContentType: "video/mp4",
-	});
-	await client.send(command);
+	})
+	await client.send(command)
 	produce(
 		JSON.stringify({ key: Key }),
 		"upload_queue",
 		"upload_exchange",
 		"file.uploaded",
-	);
-};
+	)
+}
 
-upload();
+upload()
